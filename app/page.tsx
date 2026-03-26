d1b30f622f296551209a7dcf7c6647940c5d8dde
Osama <osama.mohammed.work1@gmail.com>
2026-03-26 08:58:17 +0000

    fixed stupid bugs

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
 import Hero from "@/components/sections/Hero"
-import Skills from "@/components/sections/Skills"
-import ProjectsPreview from "@/components/sections/ProjectsPreview"
-import Contact from "@/components/sections/Contact"
+// import Skills from "@/components/sections/Skills"
+// import ProjectsPreview from "@/components/sections/ProjectsPreview"
+// import Contact from "@/components/sections/Contact"
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Skills />
-      <ProjectsPreview />
-      <Contact />
     </>
   )
 }
