387d3e1329d2f4b2a8a59d1fba07965e2f7e2229
Osama <osama.mohammed.work1@gmail.com>
2026-04-22 14:03:17 +0000

    Little changes

--- hooks/useStarField.ts
+++ hooks/useStarField.ts
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
-import { constellations } from "./constellations.data";
+import { constellations } from "@/Data/constellations"
 
 const BG_STARS = 300;
 const LERP_SPEED = 0.05;
